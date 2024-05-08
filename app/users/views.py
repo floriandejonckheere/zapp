ec82b1edb2297a495a6a57a04b9c6029422a3c19
Florian Dejonckheere <florian@floriandejonckheere.be>
2024-05-08 18:56:22 +0300

    Add /users/me endpoint

--- app/users/views.py
+++ app/users/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import Group, User
 from rest_framework import permissions, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from app.users.serializers import GroupSerializer, UserSerializer
 
@@ -11,6 +13,11 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    @action(detail=False, methods=['get'], url_path='me', url_name='me')
+    def me(self, request):
+        serializer = self.get_serializer(request.user)
+        return Response(serializer.data)
 
 
 class GroupViewSet(viewsets.ModelViewSet):
--- web/api/auth.tsx
+++ web/api/auth.tsx
@@ -4,10 +4,10 @@ export function signin(username: string, password: string) {
   return axios
     .post(
       `/api/token/`,
-      new URLSearchParams({
+      {
         username,
         password
-      }),
+      },
       {
         headers: {
           'Content-Type': 'application/json',
@@ -24,11 +24,11 @@ export function signup(email: string, username: string, password: string) {
   return axios
     .post(
       `/api/users/`,
-      new URLSearchParams({
+      {
         email,
         username,
         password
-      }),
+      },
       {
         headers: {
           'Content-Type': 'application/json',
