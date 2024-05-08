d8d156e81c877e2a60dd50c525fdcb0880c4f114
Florian Dejonckheere <florian@floriandejonckheere.be>
2024-05-08 15:34:12 +0300

    Fix unused reference

--- web/app.tsx
+++ web/app.tsx
@@ -1,9 +1,7 @@
 import { ReactElement, useEffect } from 'react'
 import { Route, Routes, useLocation } from 'react-router-dom'
-import { useLocalStorage } from '@uidotdev/usehooks'
 
 export default function App(): ReactElement {
-  const [token] = useLocalStorage('token', null)
   const location = useLocation()
 
   useEffect(() => {
