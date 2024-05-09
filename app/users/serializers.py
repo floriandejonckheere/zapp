c9824406bf8b05c64f23b7c6bac2d1aa1fd4c1a2
Florian Dejonckheere <florian@floriandejonckheere.be>
2024-05-09 10:59:32 +0300

    Allow signing up

--- app/users/serializers.py
+++ app/users/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import Group, User
 from rest_framework import serializers
 
@@ -6,6 +7,18 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'first_name', 'last_name', 'email', 'groups']
+
+
+class CreateUserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'password', 'groups']
+
+    def create(self, validated_data):
+        # Hash the password
+        validated_data['password'] = make_password(validated_data['password'])
+
+        return super().create(validated_data)
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
--- app/users/views.py
+++ app/users/views.py
@@ -1,9 +1,12 @@
+from itertools import chain
+
 from django.contrib.auth.models import Group, User
-from rest_framework import permissions, viewsets
+from rest_framework import permissions, viewsets, status
 from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
-from app.users.serializers import GroupSerializer, UserSerializer
+from app.users.serializers import GroupSerializer, CreateUserSerializer, UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -12,12 +15,38 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
+    create_serializer_class = CreateUserSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def get_serializer_class(self):
+        serializer_class = self.serializer_class
+
+        if self.request.method == 'POST':
+            serializer_class = self.create_serializer_class
+
+        return serializer_class
+
+    def get_permissions(self):
+        if self.action == 'create':
+            return [AllowAny()]
+
+        return super().get_permissions()
 
     @action(detail=False, methods=['get'], url_path='me', url_name='me')
     def me(self, request):
         serializer = self.get_serializer(request.user)
+
         return Response(serializer.data)
+
+    # @action(detail=False, methods=['post'])
+    def create(self, request):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+        else:
+            return Response({'detail': ' '.join(list(chain.from_iterable(serializer.errors.values())))}, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class GroupViewSet(viewsets.ModelViewSet):
