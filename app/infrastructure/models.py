0878d3c7eb11d7d0afdcce004a8f06013af6ed68
Florian Dejonckheere <florian@floriandejonckheere.be>
2024-05-11 10:34:32 +0300

    Add Device model

--- app/infrastructure/models.py
+++ app/infrastructure/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class Home(models.Model):
@@ -9,3 +10,17 @@ class Home(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class Device(models.Model):
+    class Category(models.TextChoices):
+        SOLAR_PANEL = "SP", _("Solar panel")
+        BATTERY = "BA", _("Battery")
+        ELECTRIC_VEHICLE = "EV", _("Electric vehicle")
+        HEAT_PUMP = "HP", _("Heat pump")
+        OTHER = "OT", _("Other")
+
+    id = models.AutoField(primary_key=True)
+    home = models.ForeignKey('Home', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    category = models.CharField(max_length=200, choices=Category.choices, default=Category.OTHER)
