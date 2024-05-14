cb6c1713c9cff960b129397071749c98b5e00f75
Florian Dejonckheere <florian@floriandejonckheere.be>
2024-05-14 21:22:06 +0300

    Add bottom menu bar

--- web/layouts/app.tsx
+++ web/layouts/app.tsx
@@ -1,58 +1,49 @@
 import React, { ReactElement } from 'react'
 import { NavLink, Outlet } from 'react-router-dom'
-import {
-  BoltIcon,
-  CalendarIcon,
-  Cog6ToothIcon
-} from '@heroicons/react/24/solid'
+import { BoltIcon, CalendarIcon } from '@heroicons/react/24/solid'
 
 export default function App(): ReactElement {
   return (
-    <div className="h-full w-full bg-yellow-400 overflow-hidden">
-      <h1 className="p-8 font-bold text-white text-3xl">Overview</h1>
+    <div className="h-full w-full bg-yellow-400">
+      <h1 className="p-8 font-semibold text-white text-2xl">Overview</h1>
 
-      <div className="h-full w-full p-8 bg-white rounded-t-3xl shadow-lg">
+      {/* White underlay */}
+      <div className="fixed top-96 h-full w-full bg-slate-50"></div>
+
+      <div className="relative h-auto flex flex-col gap-4 p-6">
         <Outlet />
       </div>
 
-      <nav className="w-full h-16 fixedpx-4 py-8 text-white font-bold flex flex-row gap-4 overflow-y-scroll">
-        <NavLink
-          to="/app/overview"
-          className={({ isActive }) =>
-            `flex items-center gap-x-2 py-2 px-3 text-sm rounded-full border-white ${
-              isActive
-                ? 'bg-white text-yellow-500 border-0'
-                : 'bg-yellow-400 text-white border-2'
-            }`
-          }
-        >
-          <BoltIcon className="inline-block h-4 w-4" /> Overview
-        </NavLink>
-        <NavLink
-          to="/app/schedule"
-          className={({ isActive }) =>
-            `flex items-center gap-x-2 py-2 px-3 text-sm rounded-full border-white ${
-              isActive
-                ? 'bg-white text-yellow-500 border-0'
-                : 'bg-yellow-400 text-white border-2'
-            }`
-          }
-        >
-          <CalendarIcon className="inline-block h-4 w-4" /> Schedule
-        </NavLink>
-        <NavLink
-          to="/app/settings"
-          className={({ isActive }) =>
-            `flex items-center gap-x-2 py-2 px-3 text-sm rounded-full border-white ${
-              isActive
-                ? 'bg-white text-yellow-500 border-0'
-                : 'bg-yellow-400 text-white border-2'
-            }`
-          }
-        >
-          <Cog6ToothIcon className="inline-block h-4 w-4" /> Settings
-        </NavLink>
-      </nav>
+      <div className="fixed bottom-0 w-full p-4 bg-slate-50 shadow-md">
+        <nav className="text-white font-bold flex flex-row justify-center  gap-4">
+          <NavLink
+            to="/overview"
+            className={({ isActive }) =>
+              `flex flex-row items-center gap-2 px-2 py-3 text-sm rounded-md ${
+                isActive
+                  ? 'bg-yellow-400 text-white'
+                  : 'bg-transparent text-yellow-400'
+              }`
+            }
+          >
+            <BoltIcon className="h-4 w-4" />
+            Overview
+          </NavLink>
+          <NavLink
+            to="/schedule"
+            className={({ isActive }) =>
+              `flex flex-row items-center gap-2 px-2 py-3 text-sm rounded-md ${
+                isActive
+                  ? 'bg-yellow-400 text-white'
+                  : 'bg-transparent text-yellow-400'
+              }`
+            }
+          >
+            <CalendarIcon className="h-4 w-4" />
+            Schedule
+          </NavLink>
+        </nav>
+      </div>
     </div>
   )
 }
--- web/pages/overview.tsx
+++ web/pages/overview.tsx
@@ -3,10 +3,37 @@ import { ReactElement } from 'react'
 export default function Overview(): ReactElement {
   return (
     <>
-      <h2 className="text-xl font-bold dark:text-slate-300">Overview</h2>
-
-      <div className="grid grid-cols-1 xl:grid-cols-2 mt-8 gap-8">
+      <div className="w-full p-6 bg-white rounded-2xl shadow-md">
         Hello, world!
+        <p>This is a paragraph</p>
+        <p>This is a paragraph</p>
+        <p>This is a paragraph</p>
+        <p>This is a paragraph</p>
+        <p>This is a paragraph</p>
+      </div>
+      <div className="w-full p-6 bg-white rounded-2xl shadow-md">
+        Hello, world!
+        <p>This is a paragraph</p>
+        <p>This is a paragraph</p>
+        <p>This is a paragraph</p>
+        <p>This is a paragraph</p>
+        <p>This is a paragraph</p>
+      </div>
+      <div className="w-full p-6 bg-white rounded-2xl shadow-md">
+        Hello, world!
+        <p>This is a paragraph</p>
+        <p>This is a paragraph</p>
+        <p>This is a paragraph</p>
+        <p>This is a paragraph</p>
+        <p>This is a paragraph</p>
+      </div>
+      <div className="w-full p-6 bg-white rounded-2xl shadow-md">
+        Hello, world!
+        <p>This is a paragraph</p>
+        <p>This is a paragraph</p>
+        <p>This is a paragraph</p>
+        <p>This is a paragraph</p>
+        <p>This is a paragraph</p>
       </div>
     </>
   )
