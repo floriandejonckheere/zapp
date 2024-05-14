36497c3568458bf04398bb469e4e86b8bcb32ee3
Florian Dejonckheere <florian@floriandejonckheere.be>
2024-05-14 22:01:21 +0300

    Add signout button

--- web/pages/settings.tsx
+++ web/pages/settings.tsx
@@ -1,6 +1,15 @@
 import { ReactElement } from 'react'
+import { useLocalStorage } from '@uidotdev/usehooks'
 
 export default function Settings(): ReactElement {
+  const [, setAccessToken] = useLocalStorage('accessToken', null)
+  const [, setRefreshToken] = useLocalStorage('refreshToken', null)
+
+  const handleSignout = () => {
+    setAccessToken(null)
+    setRefreshToken(null)
+  }
+
   return (
     <>
       <div className="w-full p-6 bg-white rounded-2xl shadow-md">
@@ -11,6 +20,22 @@ export default function Settings(): ReactElement {
         <p>This is a paragraph</p>
         <p>This is a paragraph</p>
       </div>
+
+      <div className="w-full p-6 bg-white rounded-2xl shadow-md">
+        Hello, world!
+        <p>This is a paragraph</p>
+        <p>This is a paragraph</p>
+        <p>This is a paragraph</p>
+        <p>This is a paragraph</p>
+        <p>This is a paragraph</p>
+      </div>
+
+      <button
+        onClick={handleSignout}
+        className="w-full p-4 bg-sky-700 rounded-2xl shadow-md text-center text-white"
+      >
+        Sign out
+      </button>
     </>
   )
 }
