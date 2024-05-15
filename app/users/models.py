9a65bc72921614057b9657fb72519ba06749c5c0
Florian Dejonckheere <florian@floriandejonckheere.be>
2024-05-15 20:38:39 +0300

    Add timestamps to user model

--- app/users/models.py
+++ app/users/models.py
@@ -1,7 +1,17 @@
-from django.db import models
 from django.contrib.auth.models import User as DjangoUser
+from django.db import models
 
 
 class User(models.Model):
+    # Django user
     user = models.OneToOneField(DjangoUser, on_delete=models.CASCADE)
+
+    # Homes
     homes = models.ManyToManyField('infrastructure.Home')
+
+    # Timestamps
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username
