f46439b8d2cc439f604b597162e8bc87dd41b469
Florian Dejonckheere <florian@floriandejonckheere.be>
2024-05-15 20:21:23 +0300

    Register models

--- app/infrastructure/admin.py
+++ app/infrastructure/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from .models import Home
+from .models import Home, Device, Constraint, Prediction
 
 admin.site.register(Home)
+admin.site.register(Device)
+admin.site.register(Constraint)
+admin.site.register(Prediction)
