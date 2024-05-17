137b735041a37609d1ae1b4aa9f5229686656f8d
Florian Dejonckheere <florian@floriandejonckheere.be>
2024-05-17 19:47:20 +0300

    Remove empty settings cards

--- web/pages/settings.tsx
+++ web/pages/settings.tsx
@@ -12,27 +12,9 @@ export default function Settings(): ReactElement {
 
   return (
     <>
-      <div className="w-full p-6 bg-white rounded-2xl shadow-md">
-        Hello, world!
-        <p>This is a paragraph</p>
-        <p>This is a paragraph</p>
-        <p>This is a paragraph</p>
-        <p>This is a paragraph</p>
-        <p>This is a paragraph</p>
-      </div>
-
-      <div className="w-full p-6 bg-white rounded-2xl shadow-md">
-        Hello, world!
-        <p>This is a paragraph</p>
-        <p>This is a paragraph</p>
-        <p>This is a paragraph</p>
-        <p>This is a paragraph</p>
-        <p>This is a paragraph</p>
-      </div>
-
       <button
         onClick={handleSignout}
-        className="w-full p-4 bg-sky-700 rounded-2xl shadow-md text-center text-white"
+        className="w-full p-4 bg-sky-800 rounded-2xl shadow-md text-center text-white"
       >
         Sign out
       </button>
