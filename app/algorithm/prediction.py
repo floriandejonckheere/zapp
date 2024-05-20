9d1d6fe28dd9446a92bced5e2ea9bbcf7cf51159
Florian Dejonckheere <florian@floriandejonckheere.be>
2024-05-20 11:31:10 +0300

    Remove dummy algorithm classes

--- app/algorithm/prediction.py
+++
@@ -1,9 +0,0 @@
-class Prediction:
-    def __init__(self, time, production, consumption, cost):
-        self.time = time
-        self.production = production
-        self.consumption = consumption
-        self.cost = cost
-
-    def __repr__(self):
-        return f"Prediction(time={self.time}, production={self.production}, consumption={self.consumption}, cost={self.cost})"
