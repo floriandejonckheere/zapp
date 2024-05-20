5908e95f13072af20e12aafd75db26e08d8c0368
Florian Dejonckheere <florian@floriandejonckheere.be>
2024-05-20 16:55:31 +0300

    Display schedule

--- web/pages/schedule.tsx
+++ web/pages/schedule.tsx
@@ -27,62 +27,46 @@ export default function Schedule(): ReactElement {
     enabled: !!home
   })
 
-  const deviceTypeToName = (deviceType: string) => {
-    if (deviceType === 'CO') {
-      return 'Consumer'
-    } else if (deviceType === 'PR') {
-      return 'Producer'
-    } else if (deviceType === 'ST') {
-      return 'Storage'
-    } else if (deviceType === 'GR') {
-      return 'Grid'
-    } else return 'Other'
-  }
-
   const deviceTypeToIcon = (deviceType: string) => {
     if (deviceType === 'CO') {
       // Consumer
-      return <CpuChipIcon className="inline-block mr-2 w-4 h-4 text-sky-700" />
+      return <CpuChipIcon className="inline-block mr-2 w-3 h-3 text-sky-700" />
     } else if (deviceType === 'PR') {
       // Producer
-      return <SunIcon className="inline-block mr-2 w-4 h-4 text-yellow-400" />
+      return <SunIcon className="inline-block mr-2 w-3 h-3 text-yellow-400" />
     } else if (deviceType === 'ST') {
       // Storage
       return (
-        <Battery100Icon className="inline-block mr-2 w-4 h-4 text-green-400" />
+        <Battery100Icon className="inline-block mr-2 w-3 h-3 text-green-400" />
       )
     } else {
-      return <BoltIcon className="inline-block mr-2 w-4 h-4 text-sky-700" />
+      return <BoltIcon className="inline-block mr-2 w-3 h-3 text-sky-700" />
     }
   }
 
   if (!home) return <div>Loading...</div>
 
   return (
-    <>
+    <div className="w-full p-6 bg-white rounded-2xl shadow-md flex flex-col gap-6">
       {isSuccess &&
         data[0].elements
           .filter((element) => element.device.deviceType != 'GR')
           .map((element) => (
-            <div
-              key={element.id}
-              className="w-full p-6 bg-white rounded-2xl shadow-md flex flex-col gap-3"
-            >
+            <div key={element.id} className="flex flex-col">
               <div className="flex items-baseline">
                 {deviceTypeToIcon(element.device.deviceType)}
-                <div>
-                  <div className="font-medium text-lg">
-                    {element.device.name}
-                  </div>
-                </div>
-
-                <div className="flex-1" />
-                <div className="text-gray-500 text-xs">
-                  {deviceTypeToName(element.device.deviceType)}
-                </div>
+                <div className="font-medium text-sm">{element.device.name}</div>
+              </div>
+              <div className="w-full flex">
+                {element.power.map((power, index) => (
+                  <div
+                    key={index}
+                    className={`w-[4%] h-5 ${power > 0 ? 'bg-green-400' : power < 0 ? 'bg-red-400' : 'bg-gray-400'}`}
+                  ></div>
+                ))}
               </div>
             </div>
           ))}
-    </>
+    </div>
   )
 }
