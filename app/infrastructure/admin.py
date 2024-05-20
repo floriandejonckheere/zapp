481218ce77975746c750430e650898848772f5b6
Florian Dejonckheere <florian@floriandejonckheere.be>
2024-05-20 11:54:08 +0300

    Move some classes into schedule app

--- app/infrastructure/admin.py
+++ app/infrastructure/admin.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
 
-from .models import Home, Device, Constraint, Prediction, Schedule, ScheduleElement
+from .models import Home, Device, Constraint
 
 admin.site.register(Home)
 admin.site.register(Device)
 admin.site.register(Constraint)
-admin.site.register(Prediction)
-admin.site.register(Schedule)
-admin.site.register(ScheduleElement)
---
+++ app/schedule/models.py
@@ -0,0 +1,62 @@
+from django.contrib.postgres.fields import ArrayField
+from django.db import models
+
+
+class Prediction(models.Model):
+    # Primary key
+    id = models.AutoField(primary_key=True)
+
+    # Home
+    home = models.ForeignKey('infrastructure.Home', on_delete=models.CASCADE)
+
+    # Date
+    date = models.DateField()
+
+    # Predicted energy production (W), per hour
+    production = ArrayField(models.IntegerField(), size=24)
+
+    # Predicted energy consumption (W), per hour
+    consumption = ArrayField(models.IntegerField(), size=24)
+
+    # Predicted cost (c€/kWh), per hour
+    cost = ArrayField(models.FloatField(), size=24)
+
+    # Timestamps
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.home.name} ({self.date})'
+
+
+class Schedule(models.Model):
+    # Primary key
+    id = models.AutoField(primary_key=True)
+
+    # Home
+    home = models.ForeignKey('infrastructure.Home', on_delete=models.CASCADE)
+
+    # Date
+    date = models.DateField()
+
+    # Timestamps
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class ScheduleElement(models.Model):
+    # Primary key
+    id = models.AutoField(primary_key=True)
+
+    # Schedule
+    schedule = models.ForeignKey('Schedule', on_delete=models.CASCADE)
+
+    # Device
+    device = models.ForeignKey('infrastructure.Device', on_delete=models.CASCADE)
+
+    # Predicted energy consumption (W), per hour
+    power = ArrayField(models.IntegerField(), size=24)
+
+    # Timestamps
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
