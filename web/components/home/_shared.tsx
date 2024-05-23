847cadc8853cec2eae1e862f93098705773543bc
Florian Dejonckheere <florian@floriandejonckheere.be>
2024-05-23 17:23:49 +0300

    Extract device cards

---
+++ web/components/home/_shared.tsx
@@ -0,0 +1,36 @@
+import {
+  Battery100Icon,
+  BoltIcon,
+  CpuChipIcon,
+  SunIcon
+} from '@heroicons/react/24/solid'
+import React from 'react'
+
+export const deviceTypeToName = (deviceType: string) => {
+  if (deviceType === 'CO') {
+    return 'Consumer'
+  } else if (deviceType === 'PR') {
+    return 'Producer'
+  } else if (deviceType === 'ST') {
+    return 'Storage'
+  } else if (deviceType === 'GR') {
+    return 'Grid'
+  } else return 'Other'
+}
+
+export const deviceTypeToIcon = (deviceType: string) => {
+  if (deviceType === 'CO') {
+    // Consumer
+    return <CpuChipIcon className="inline-block mr-2 w-4 h-4 text-sky-700" />
+  } else if (deviceType === 'PR') {
+    // Producer
+    return <SunIcon className="inline-block mr-2 w-4 h-4 text-yellow-400" />
+  } else if (deviceType === 'ST') {
+    // Storage
+    return (
+      <Battery100Icon className="inline-block mr-2 w-4 h-4 text-green-400" />
+    )
+  } else {
+    return <BoltIcon className="inline-block mr-2 w-4 h-4 text-sky-700" />
+  }
+}
--- web/pages/home.tsx
+++ web/pages/home.tsx
@@ -1,72 +1,14 @@
 import React, { ReactElement } from 'react'
 import { useQuery } from '@tanstack/react-query'
-import {
-  Battery100Icon,
-  BoltIcon,
-  ClockIcon,
-  CpuChipIcon,
-  CurrencyDollarIcon,
-  SunIcon
-} from '@heroicons/react/24/solid'
-
-import { Constraint } from '@/types'
 
 import { useHome } from '@/contexts/home'
 
-import { getConstraints, getDevices } from '@/api/infrastructure'
+import Consumer from '@/components/home/consumer'
+import Producer from '@/components/home/producer'
+import Storage from '@/components/home/storage'
+import Grid from '@/components/home/grid'
 
-function Constraints(props: { homeId: number; deviceId: number }) {
-  const { homeId, deviceId } = props
-
-  const { isSuccess, data } = useQuery({
-    queryKey: ['constraints', homeId, deviceId],
-    queryFn: () => getConstraints(homeId, deviceId),
-    enabled: !!homeId && !!deviceId
-  })
-
-  if (!isSuccess) {
-    return <div>Loading...</div>
-  }
-
-  return data.map((constraint: Constraint) => {
-    if (constraint.constraintType === 'TI') {
-      // Time
-      return (
-        <div key={constraint.id} className="text-sm">
-          <ClockIcon className="inline-block mr-2 w-4 h-4 text-sky-700" />
-          {constraint.start != null && `From ${constraint.start}:00 `}
-          {constraint.stop != null && `Until ${constraint.stop}:00`}
-        </div>
-      )
-    } else if (constraint.constraintType === 'CO') {
-      // Cost
-      return (
-        <div key={constraint.id} className="text-sm">
-          <CurrencyDollarIcon className="inline-block mr-2 w-4 h-4 text-sky-700" />
-          {constraint.start != null && `From ${constraint.start} c€/kWh `}
-          {constraint.stop != null && `Until ${constraint.stop} c€/kWh`}
-        </div>
-      )
-    } else if (constraint.constraintType === 'SO') {
-      // Source
-      return (
-        <div key={constraint.id} className="text-sm">
-          <BoltIcon className="inline-block mr-2 w-4 h-4 text-sky-700" />
-          {constraint.sourceId}
-        </div>
-      )
-    } else if (constraint.constraintType === 'PO') {
-      // Power
-      return (
-        <div key={constraint.id} className="text-sm">
-          <BoltIcon className="inline-block mr-2 w-4 h-4 text-sky-700" />
-          {/* @ts-expect-error error */}
-          Maximum {constraint.stop / 1000} kW
-        </div>
-      )
-    }
-  })
-}
+import { getDevices } from '@/api/infrastructure'
 
 export default function Home(): ReactElement {
   const { home } = useHome()
@@ -77,35 +19,6 @@ export default function Home(): ReactElement {
     queryFn: () => getDevices(home.id),
     enabled: !!home
   })
-
-  const deviceTypeToName = (deviceType: string) => {
-    if (deviceType === 'CO') {
-      return 'Consumer'
-    } else if (deviceType === 'PR') {
-      return 'Producer'
-    } else if (deviceType === 'ST') {
-      return 'Storage'
-    } else if (deviceType === 'GR') {
-      return 'Grid'
-    } else return 'Other'
-  }
-
-  const deviceTypeToIcon = (deviceType: string) => {
-    if (deviceType === 'CO') {
-      // Consumer
-      return <CpuChipIcon className="inline-block mr-2 w-4 h-4 text-sky-700" />
-    } else if (deviceType === 'PR') {
-      // Producer
-      return <SunIcon className="inline-block mr-2 w-4 h-4 text-yellow-400" />
-    } else if (deviceType === 'ST') {
-      // Storage
-      return (
-        <Battery100Icon className="inline-block mr-2 w-4 h-4 text-green-400" />
-      )
-    } else {
-      return <BoltIcon className="inline-block mr-2 w-4 h-4 text-sky-700" />
-    }
-  }
 
   if (!home) return <div>Loading...</div>
 
@@ -119,23 +32,18 @@ export default function Home(): ReactElement {
               key={device.id}
               className="w-full p-6 bg-white rounded-2xl shadow-md flex flex-col gap-3"
             >
-              <div className="flex items-baseline">
-                {deviceTypeToIcon(device.deviceType)}
-                <div>
-                  <div className="font-medium text-lg">{device.name}</div>
-                  <div className="text-gray-500 text-sm">
-                    {device.power && `${device.power / 1000} kW`}
-                    {device.power && device.capacity && ', '}
-                    {device.capacity && `${device.capacity / 1000} kWh`}
-                  </div>
-                </div>
-
-                <div className="flex-1" />
-                <div className="text-gray-500 text-xs">
-                  {deviceTypeToName(device.deviceType)}
-                </div>
-              </div>
-              <Constraints homeId={home.id} deviceId={device.id} />
+              {device.deviceType === 'CO' && (
+                <Consumer home={home} device={device} />
+              )}
+              {device.deviceType === 'PR' && (
+                <Producer home={home} device={device} />
+              )}
+              {device.deviceType === 'ST' && (
+                <Storage home={home} device={device} />
+              )}
+              {device.deviceType === 'GR' && (
+                <Grid home={home} device={device} />
+              )}
             </div>
           ))}
     </>
