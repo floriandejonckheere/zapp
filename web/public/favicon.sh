4cbe52e8ad4a8dd7d047cec2e786ae8e663eed04
Florian Dejonckheere <florian@floriandejonckheere.be>
2024-05-23 10:48:41 +0300

    Update manifest and icons

--- web/public/favicon.sh
+++ web/public/favicon.sh
@@ -11,8 +11,8 @@ convert -background none -resize 32x32 ${SOURCE} /tmp/favicon-32.png
 convert -background none -resize 64x64 ${SOURCE} /tmp/favicon-64.png
 convert /tmp/favicon-16.png /tmp/favicon-32.png /tmp/favicon-64.png ${FAVICON}
 
-convert -background none -resize 192x192 ${SOURCE} logo192.png
-convert -background none -resize 256x256 ${SOURCE} logo256.png
-convert -background none -resize 512x512 ${SOURCE} logo512.png
+convert -background none -density 2000 -resize 192x192 ${SOURCE} logo192.png
+convert -background none -density 2000 -resize 256x256 ${SOURCE} logo256.png
+convert -background none -density 2000 -resize 512x512 ${SOURCE} logo512.png
 
 echo "Generated ${FAVICON}"
