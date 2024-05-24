3151f75937d2ae447efdd6d257ee43b7e60a633f
Florian Dejonckheere <florian@floriandejonckheere.be>
2024-05-24 11:34:36 +0300

    Display grid and use shared function

--- web/components/schedule/schedule_card.tsx
+++ web/components/schedule/schedule_card.tsx
@@ -1,16 +1,11 @@
 import React, { ReactElement } from 'react'
 import { useQuery } from '@tanstack/react-query'
-import {
-  Battery100Icon,
-  BoltIcon,
-  CpuChipIcon,
-  SunIcon
-} from '@heroicons/react/24/solid'
 
 import { dateTypeToAPIString } from '@/utils'
 import { DateType, Home } from '@/types'
 
 import { getSchedules } from '@/api/schedule'
+import { deviceTypeToIcon } from '@/components/home/_shared'
 
 export default function ScheduleCard(props: {
   home: Home
@@ -23,23 +18,6 @@ export default function ScheduleCard(props: {
     queryFn: () => getSchedules(home.id, dateTypeToAPIString(date)),
     enabled: !!home
   })
-
-  const deviceTypeToIcon = (deviceType: string) => {
-    if (deviceType === 'CO') {
-      // Consumer
-      return <CpuChipIcon className="inline-block mr-2 w-3 h-3 text-sky-700" />
-    } else if (deviceType === 'PR') {
-      // Producer
-      return <SunIcon className="inline-block mr-2 w-3 h-3 text-yellow-400" />
-    } else if (deviceType === 'ST') {
-      // Storage
-      return (
-        <Battery100Icon className="inline-block mr-2 w-3 h-3 text-green-400" />
-      )
-    } else {
-      return <BoltIcon className="inline-block mr-2 w-3 h-3 text-sky-700" />
-    }
-  }
 
   if (!isSuccess)
     return (
@@ -54,27 +32,25 @@ export default function ScheduleCard(props: {
 
   return (
     <div className="w-full p-6 bg-white rounded-2xl shadow-md flex flex-col gap-6">
-      {data[0].elements
-        .filter((element) => element.device.deviceType != 'GR')
-        .map((element) => (
-          <div key={element.id} className="flex flex-col">
-            <div className="flex items-baseline">
-              {deviceTypeToIcon(element.device.deviceType)}
-              <div className="font-medium text-sm">{element.device.name}</div>
-            </div>
-            <div className="w-full flex gap-0.5">
-              {element.power.map((power, index) => (
-                <div
-                  key={index}
-                  className={`w-[4%] h-5 text-xs  text-center ${power > 0 ? 'bg-green-400' : power < 0 ? 'bg-red-400' : 'bg-gray-400'}`}
-                >
-                  {/* TODO: remove for final version */}
-                  {power}
-                </div>
-              ))}
-            </div>
+      {data[0].elements.map((element) => (
+        <div key={element.id} className="flex flex-col">
+          <div className="flex items-baseline">
+            {deviceTypeToIcon(element.device.deviceType)}
+            <div className="font-medium text-sm">{element.device.name}</div>
           </div>
-        ))}
+          <div className="w-full flex gap-0.5">
+            {element.power.map((power, index) => (
+              <div
+                key={index}
+                className={`w-[4%] h-5 text-xs  text-center ${power > 0 ? 'bg-green-400' : power < 0 ? 'bg-red-400' : 'bg-gray-400'}`}
+              >
+                {/* TODO: remove for final version */}
+                {power}
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
