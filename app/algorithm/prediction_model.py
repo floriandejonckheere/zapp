e85fb713d0599a86ca9634ffeff7a1b525a68024
Joni <59820282+Jonitsi@users.noreply.github.com>
2024-05-24 15:25:59 +0300

    Update prediction_model.py
    fixes

--- app/algorithm/prediction_model.py
+++ app/algorithm/prediction_model.py
@@ -1,17 +1,14 @@
 import requests
-import json
 import pandas as pd
 import pytz
 import numpy as np
 import os
-import warnings
 
 from datetime import datetime, timedelta
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-from sklearn import metrics
 
 #PREDICTION DONE BY CALLING Predict_labels()
 
@@ -186,11 +183,11 @@ def train_and_evaluate_model():
     model = RandomForestRegressor(n_estimators=100, random_state=42)
     model.fit(X_train, y_train)
 
-    y_pred = model.predict(X_test)
-    mae = mean_absolute_error(y_test, y_pred)
-    mse = mean_squared_error(y_test, y_pred)
-    rmse = np.sqrt(mse)
-    r2 = r2_score(y_test, y_pred)
+    #y_pred = model.predict(X_test)
+    #mae = mean_absolute_error(y_test, y_pred)
+    #mse = mean_squared_error(y_test, y_pred)
+    #rmse = np.sqrt(mse)
+    #r2 = r2_score(y_test, y_pred)
 
     #print(f"Mean Absolute Error: {mae}")
     #print(f"Mean Squared Error: {mse}")
