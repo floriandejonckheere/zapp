b73bfffec9288f7761f16ac78d8a6cf80a918d37
Ottispottis <osheld@utu.fi>
2024-05-26 18:04:01 +0300

    fixes

--- web/pages/overview.tsx
+++ web/pages/overview.tsx
@@ -19,7 +19,7 @@ export default function Overview(): ReactElement {
         </div>
 
         {/* PV */}
-        <div className="absolute flex flex-col items-center top-1/4 text-center text-yellow-500">
+        <div className="absolute flex flex-col items-center top-14 text-center text-yellow-500 z-10">
           <div className=" text-gray-300">100 W</div>
           <div className=" text-gray-500">Rest</div>
           <div className="border-2 border-yellow-500 rounded-full h-10 w-10 flex items-center justify-center bg-neutral-800">
@@ -28,7 +28,7 @@ export default function Overview(): ReactElement {
         </div>
 
         {/* Battery */}
-        <div className="absolute flex flex-col items-center top-1/4 right-2 text-center text-green-500">
+        <div className="absolute flex flex-col items-center top-14 right-2 text-center text-green-500 z-10">
           <div className=" text-gray-300">2.3 kW</div>
           <div className=" text-gray-500">PV</div>
           <div className="border-2 border-green-500 rounded-full h-10 w-10 flex items-center justify-center bg-neutral-800">
@@ -37,7 +37,7 @@ export default function Overview(): ReactElement {
         </div>
 
         {/* House */}
-        <div className="absolute flex flex-col items-center bottom-1/4 right-2 text-center text-yellow-500">
+        <div className="absolute flex flex-col items-center bottom-1/4 right-2 text-center text-yellow-500 z-10">
           <div className=" text-gray-300">1.2 kW</div>
           <div className=" text-gray-500">Battery</div>
           <div className="border-2 border-yellow-500 rounded-full h-10 w-10 flex items-center justify-center bg-neutral-800">
@@ -46,7 +46,7 @@ export default function Overview(): ReactElement {
         </div>
 
         {/* Wallbox */}
-        <div className="absolute flex flex-col items-center bottom-2 left-1/2 transform -translate-x-1/2 text-center text-gray-300">
+        <div className="absolute flex flex-col items-center bottom-2 left-1/2 transform -translate-x-1/2 text-center text-gray-300 z-10">
           <div className="border-2 border-gray-500 rounded-full h-10 w-10 flex items-center justify-center bg-neutral-800">
             <BsFillPlugFill className="text-gray-300" />
           </div>
@@ -55,7 +55,7 @@ export default function Overview(): ReactElement {
         </div>
 
         {/* Rest */}
-        <div className="absolute flex flex-col items-center bottom-1/4  text-center text-yellow-500">
+        <div className="absolute flex flex-col items-center bottom-1/4  text-center text-yellow-500 z-10">
           <div className=" text-gray-300">100 W</div>
           <div className=" text-gray-500">House</div>
           <div className="border-2 border-yellow-500 rounded-full h-10 w-10 flex items-center justify-center bg-neutral-800">
@@ -75,13 +75,45 @@ export default function Overview(): ReactElement {
           {/* Diagonal Lines */}
           <div className="absolute w-full h-full">
             {/* Top-Left to Center */}
-            <div className="absolute w-0.5 h-1/2 bg-gray-300 transform rotate-45 origin-bottom-left left-1/2 top-0"></div>
+            <div
+              className="absolute w-0.5 h-1/3 bg-gray-300 transform"
+              style={{
+                transform: 'rotate(60deg)',
+                transformOrigin: 'bottom left',
+                left: '50%',
+                top: '16.5%'
+              }}
+            ></div>
             {/* Top-Right to Center */}
-            <div className="absolute w-0.5 h-1/2 bg-gray-300 transform -rotate-45 origin-bottom-right right-1/2 top-0"></div>
+            <div
+              className="absolute w-0.5 h-1/3 bg-gray-300 transform "
+              style={{
+                transform: 'rotate(-60deg)',
+                transformOrigin: 'bottom right',
+                left: '50%',
+                top: '16.5%'
+              }}
+            ></div>
             {/* Bottom-Left to Center */}
-            <div className="absolute w-0.5 h-1/2 bg-gray-300 transform -rotate-45 origin-top-left left-1/2 bottom-0"></div>
+            <div
+              className="absolute w-0.5 h-1/3 bg-gray-300 transform"
+              style={{
+                transform: 'rotate(-60deg)',
+                transformOrigin: 'top left',
+                left: '50%',
+                top: '50%'
+              }}
+            ></div>
             {/* Bottom-Right to Center */}
-            <div className="absolute w-0.5 h-1/2 bg-gray-300 transform rotate-45 origin-top-right right-1/2 bottom-0"></div>
+            <div
+              className="absolute w-0.5 h-1/3 bg-yellow-300 transform"
+              style={{
+                transform: 'rotate(60deg)',
+                transformOrigin: 'top right',
+                left: '50%',
+                top: '50%'
+              }}
+            ></div>
           </div>
         </div>
       </div>
